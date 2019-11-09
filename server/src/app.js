5a9a5dca44942f905b59820ca82f6c8afe92e55c
Jakub Janik <jakubjanik@vp.pl>
2019-11-09 20:46:05 +0100

    Replace express-session with cookie-session
    Get rid of heroku warnings

--- server/src/app.js
+++ server/src/app.js
@@ -3,7 +3,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import session from 'express-session';
+import session from 'cookie-session';
 import passport from 'passport';
 import path from 'path';
 import chatRoutes from './routes/chat';
